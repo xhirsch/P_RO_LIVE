3ff44e5fac7319fe183d536f16cfca8505f31033
Xaver Hirsch <34938604+xhirsch@users.noreply.github.com>
2022-01-21 16:29:07 +0100

    add drone

---
+++ Assets/Code/Scripts/IP_InPlace_Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IndiePixel
+{
+    public class IP_InPlace_Camera : MonoBehaviour
+    {
+        #region Variables
+        [Header("Camera Properties")]
+        public Transform target;
+        #endregion
+
+
+        void FixedUpdate()
+        {
+            if(!target)
+            {
+                return;
+            }
+
+            transform.LookAt(target);
+        }
+    }
+}
